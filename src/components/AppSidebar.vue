5f9ccb737f512cc4bac2121d728307b28f455824
Administrator <admin@douyin-live-info.local>
2026-07-06 21:46:54 +0800

    v2.5.1: 修复关闭报错+刷新按钮+清除登录，打包发布

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -23,7 +23,7 @@
       </button>
     </div>
     <div class="sidebar-footer">
-      <div class="version-tag">V 2.5.0</div>
+      <div class="version-tag">V 2.5.1</div>
     </div>
   </div>
 </template>
